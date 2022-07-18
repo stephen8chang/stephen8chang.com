5aae58ae2952cb40eff1a44d29938e3781526e32
Stephen Chang <stephen8chang@gmail.com>
2022-07-18 16:43:18 -0500

    Finish About, education, work fields

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -13,8 +13,7 @@ class Contact extends Component {
       var email = this.props.data.email;
       var message = this.props.data.contactmessage;
     }
-
-    return (
+   return (
       <section id="contact">
 
          <div className="row section-head">
@@ -32,86 +31,86 @@ class Contact extends Component {
             </div>
 
          </div>
+      </section>
+   );
+   //       <div className="row">
+   //          <div className="eight columns">
 
-         <div className="row">
-            <div className="eight columns">
+   //             <form action="" method="post" id="contactForm" name="contactForm">
+	// 				<fieldset>
 
-               <form action="" method="post" id="contactForm" name="contactForm">
-					<fieldset>
+   //                <div>
+	// 					   <label htmlFor="contactName">Name <span className="required">*</span></label>
+	// 					   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
+   //                </div>
 
-                  <div>
-						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
-                  </div>
+   //                <div>
+	// 					   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
+	// 					   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
+   //                </div>
 
-                  <div>
-						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
-                  </div>
+   //                <div>
+	// 					   <label htmlFor="contactSubject">Subject</label>
+	// 					   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
+   //                </div>
 
-                  <div>
-						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
-                  </div>
+   //                <div>
+   //                   <label htmlFor="contactMessage">Message <span className="required">*</span></label>
+   //                   <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+   //                </div>
 
-                  <div>
-                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                  </div>
+   //                <div>
+   //                   <button className="submit">Submit</button>
+   //                   <span id="image-loader">
+   //                      <img alt="" src="images/loader.gif" />
+   //                   </span>
+   //                </div>
+	// 				</fieldset>
+	// 			   </form>
 
-                  <div>
-                     <button className="submit">Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
-                  </div>
-					</fieldset>
-				   </form>
-
-           <div id="message-warning"> Error boy</div>
-				   <div id="message-success">
-                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
-           </div>
+   //         <div id="message-warning"> Error boy</div>
+	// 			   <div id="message-success">
+   //                <i className="fa fa-check"></i>Your message was sent, thank you!<br />
+	// 			   </div>
+   //         </div>
 
 
-            <aside className="four columns footer-widgets">
-               <div className="widget widget_contact">
+   //          <aside className="four columns footer-widgets">
+   //             <div className="widget widget_contact">
 
-					   <h4>Address and Phone</h4>
-					   <p className="address">
-						   {name}<br />
-						   {street} <br />
-						   {city}, {state} {zip}<br />
-						   <span>{phone}</span>
-					   </p>
-				   </div>
+	// 				   <h4>Address and Phone</h4>
+	// 				   <p className="address">
+	// 					   {name}<br />
+	// 					   {street} <br />
+	// 					   {city}, {state} {zip}<br />
+	// 					   <span>{phone}</span>
+	// 				   </p>
+	// 			   </div>
 
-               <div className="widget widget_tweets">
-                  <h4 className="widget-title">Latest Tweets</h4>
-                  <ul id="twitter">
-                     <li>
-                        <span>
-                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                        Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">2 Days Ago</a></b>
-                     </li>
-                     <li>
-                        <span>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                        eaque ipsa quae ab illo inventore veritatis et quasi
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">3 Days Ago</a></b>
-                     </li>
-                  </ul>
-		         </div>
-            </aside>
-      </div>
-   </section>
-    );
+   //             <div className="widget widget_tweets">
+   //                <h4 className="widget-title">Latest Tweets</h4>
+   //                <ul id="twitter">
+   //                   <li>
+   //                      <span>
+   //                      This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+   //                      Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
+   //                      <a href="#">http://t.co/CGIrdxIlI3</a>
+   //                      </span>
+   //                      <b><a href="#">2 Days Ago</a></b>
+   //                   </li>
+   //                   <li>
+   //                      <span>
+   //                      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+   //                      eaque ipsa quae ab illo inventore veritatis et quasi
+   //                      <a href="#">http://t.co/CGIrdxIlI3</a>
+   //                      </span>
+   //                      <b><a href="#">3 Days Ago</a></b>
+   //                   </li>
+   //                </ul>
+	// 	         </div>
+   //          </aside>
+   //    </div>
+   // </section>
   }
 }
 
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -45,7 +45,7 @@ class Resume extends Component {
          <div className="three columns header-col">
             <h1><span>Work</span></h1>
          </div>
-
+        
          <div className="nine columns main-col">
           {work}
         </div>
