bdbc0d93a8f1037f576b16d5a949da0f47c9d538
Stephen Chang <stephen8chang@gmail.com>
2022-07-20 21:53:34 -0500

    add image carousel component

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
 
 class Testimonials extends Component {
   render() {
@@ -15,22 +18,40 @@ class Testimonials extends Component {
     }
 
     return (
-      <section id="testimonials">
-      <div className="text-container">
-         <div className="row">
-
-            <div className="two columns header-col">
-               <h1><span>Client Testimonials</span></h1>
-            </div>
-
-            <div className="ten columns flex-container">
-                  <ul className="slides">
-                      {testimonials}
-                  </ul>
-               </div>
-            </div>
+      <section id="testimonials" style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+         <div className="text-container" style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <Carousel thumbWidth={0} width={'100%'}>
+                <div style={{width:300,marginLeft:"auto",marginRight:"auto"}}>
+                    <img src="https://i.imgur.com/s7zvCCE.jpg" />
+                    <p className="legend1">Legend 1</p>
+                </div>
+                <div style={{width:300,marginLeft:"auto",marginRight:"auto"}}>
+                    <img src="https://i.imgur.com/PolZoW5.jpg" />
+                    <p className="legend2">Legend 2</p>
+                </div>
+                <div style={{width:300,marginLeft:"auto",marginRight:"auto"}}>
+                    <img src="https://i.imgur.com/Lub4L6j.jpg" />
+                    <p className="legend3">Legend 3</p>
+                </div>
+                <div style={{width:300,marginLeft:"auto",marginRight:"auto"}}>
+                    <img src="https://i.imgur.com/b7jvKyv.jpg" />
+                    <p className="legend3">Legend 4</p>
+                </div>
+                <div style={{width:300,marginLeft:"auto",marginRight:"auto"}}>
+                    <img src="https://i.imgur.com/NRWatXU.jpg" />
+                    <p className="legend3">Legend 5</p>
+                </div>
+                <div style={{width:300,marginLeft:"auto",marginRight:"auto"}}>
+                    <img src="https://i.imgur.com/6BlSX7R.jpg" />
+                    <p className="legend3">Legend 6</p>
+                </div>
+                <div style={{width:300,marginLeft:"auto",marginRight:"auto"}}>
+                    <img src="https://i.imgur.com/zu7WLWz.jpg" />
+                    <p className="legend3">Legend 7</p>
+                </div>
+            </Carousel>
          </div>
-   </section>
+      </section>
     );
   }
 }
